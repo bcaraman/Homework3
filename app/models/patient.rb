bc9b6ab350600ee8feb07220b410304224ae95fc
bcaraman <briancaramanianuh@gmail.com>
2019-10-28 19:13:29 -0500

    final Hw3

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,6 @@
 class Patient < ApplicationRecord
+  has_many :offices, inverse_of: :physician
+  accepts_nested_attributes_for :offices, reject_if: :all_blank, allow_destroy: true
+
+  has_and_belongs_to_many :physicians, through: :appointment
 end
--- app/models/physician.rb
+++ app/models/physician.rb
@@ -1,5 +1,7 @@
 class Physician < ApplicationRecord
   has_many :offices, inverse_of: :physician
   accepts_nested_attributes_for :offices, reject_if: :all_blank, allow_destroy: true
+
+  has_and_belongs_to_many :patients, through: :appointment
 end
 
